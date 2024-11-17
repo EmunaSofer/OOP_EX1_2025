f441169d28a96be8822eb54149e482da5a12a67b
EmunaSofer <emuna002@gmail.com>
2024-11-17 21:10:36 +0200

    נפתח לך?

--- GameLogic.java
+++ GameLogic.java
@@ -1,25 +1,53 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameLogic implements PlayableLogic{
 
+    private Player player1;
+    private Player player2;
+    private Disc[][] board;
+    private final int boardSize=8;
+    private boolean firstPlayerTurn;
+
+    public GameLogic()
+    {
+        this.board=new Disc[boardSize][boardSize];
+        this.firstPlayerTurn=true;
+    }
+    //להוסיף מצב של אותו דבר ליד
     @Override
-    public boolean locate_disc(Position a, Disc disc) {
+    public boolean locate_disc(Position a, Disc disc)
+    {
+        if (isValidPosition(a)&&board[a.row()][a.col]==null&& countFlips(a)>0) {
+        board[a.row()][a.col]=disc;
+        return true;
+        }
         return false;
     }
 
     @Override
-    public Disc getDiscAtPosition(Position position) {
-        return null;
+    public Disc getDiscAtPosition(Position position)
+    {
+        return isValidPosition(position)? board[position.row()][position.col()]:null;
     }
 
     @Override
     public int getBoardSize() {
-        return 0;
+        return this.boardSize;
     }
 
     @Override
     public List<Position> ValidMoves() {
-        return List.of();
+        List<Position> validMoves = new ArrayList<>();
+        for (int row = 0; row < boardSize; row++) {
+            for (int col = 0; col < boardSize; col++) {
+                Position pos = new Position(row, col);
+                if (board[row][col] == null) {
+                    validMoves.add(pos);
+                }
+            }
+        }
+        return validMoves;
     }
 
     @Override
@@ -29,36 +57,44 @@ public class GameLogic implements PlayableLogic{
 
     @Override
     public Player getFirstPlayer() {
-        return null;
+        return this.player1;
     }
 
     @Override
     public Player getSecondPlayer() {
-        return null;
+        return this.player2;
     }
 
     @Override
     public void setPlayers(Player player1, Player player2) {
-
+        this.player1=player1;
+        this.player2=player2;
     }
 
     @Override
     public boolean isFirstPlayerTurn() {
-        return false;
+        return this.firstPlayerTurn;
     }
 
     @Override
     public boolean isGameFinished() {
-        return false;
-    }
 
+        return ValidMoves().isEmpty();
+    }
+    //צריך להוסיף את השחקנים ההתחלתים
     @Override
     public void reset() {
-
+        this.board = new Disc[boardSize][boardSize];
+        this.firstPlayerTurn = true;
     }
 
     @Override
     public void undoLastMove() {
+
+    }
+    public boolean isValidPosition(Position pos)
+    {
+        return pos.col()>=0 && pos.col()<8 && pos.row()>=0 && pos.row()<8;
 
     }
 }
